2dbe904f2730709624669fbd9e48dff45e1d5546
Robin Wen <blockxyz@gmail.com>
2021-08-22 17:57:00 +0800

    Fix bug

--- snapshots/bytom_snapshots.sh
+++ snapshots/bytom_snapshots.sh
@@ -20,10 +20,10 @@ webhook_url="$(config_get WEBHOOK_URL)"
 access_token="$(config_get ACCESS_TOKEN)"
 
 # votes snapshots
-curl https://bcapi.movapi.com/blockmeta/bytom2/v1/node/vote-transactions\?pub_key\=cc499725c708f272b3111c9e0694b66bba61f0321b14ee07b41cd677c9a633259bdbc96b9e67097d026dc9d4f859967194fd6e1770bf5cb6785c388d18486d80\&start\=0\&limit\=200 | jq > ${base_dir}/votes-`date '+%Y%m%d'`.log
+curl https://bcapi.movapi.com/blockmeta/bytom2/v1/node/vote-transactions\?pub_key\=cc499725c708f272b3111c9e0694b66bba61f0321b14ee07b41cd677c9a633259bdbc96b9e67097d026dc9d4f859967194fd6e1770bf5cb6785c388d18486d80\&start\=0\&limit\=200 | jq '.' > ${base_dir}/votes-`date '+%Y%m%d'`.log
 
 # balance snapshots
-curl https://bcapi.movapi.com/blockmeta/bytom2/v1/address\?address\=bn1q3hqjn68pj9r5uhdwqsve3xwu6x2rlla2w8er0p | jq > ${base_dir}/balance-`date '+%Y%m%d'`.log
+curl https://bcapi.movapi.com/blockmeta/bytom2/v1/address\?address\=bn1q3hqjn68pj9r5uhdwqsve3xwu6x2rlla2w8er0p | jq '.' > ${base_dir}/balance-`date '+%Y%m%d'`.log
 
 log="时间: `date '+%Y-%m-%d %H:%M:%S'` UTC \n主机名: `hostname` \n节点: $service \n状态: 快照已完成。"
 echo -e $log >> $log_file
