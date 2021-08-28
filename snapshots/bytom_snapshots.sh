f7acaaa6eaa6cc970807c9520a0989a4feb6b370
Robin Wen <blockxyz@gmail.com>
2021-08-28 14:15:58 +0800

    Add all votes

--- snapshots/bytom_snapshots.sh
+++ snapshots/bytom_snapshots.sh
@@ -26,7 +26,7 @@ curl https://bcapi.movapi.com/blockmeta/bytom2/v1/node/vote-transactions\?pub_ke
 curl https://bcapi.movapi.com/blockmeta/bytom2/v1/address\?address\=bn1q3hqjn68pj9r5uhdwqsve3xwu6x2rlla2w8er0p | jq '.' > ${base_dir}/balance-`date '+%Y%m%d'`.log
 
 # allvotes snapshots
-curl https://bcapi.movapi.com/blockmeta/bytom2/v1/node/detail?pub_key=cc499725c708f272b3111c9e0694b66bba61f0321b14ee07b41cd677c9a633259bdbc96b9e67097d026dc9d4f859967194fd6e1770bf5cb6785c388d18486d80 | jq '.' > ${base_dir}/allvotes-`date '+%Y%m%d'`.log
+curl https://bcapi.movapi.com/blockmeta/bytom2/v1/node/detail\?pub_key\=cc499725c708f272b3111c9e0694b66bba61f0321b14ee07b41cd677c9a633259bdbc96b9e67097d026dc9d4f859967194fd6e1770bf5cb6785c388d18486d80 | jq '.' > ${base_dir}/allvotes-`date '+%Y%m%d'`.log
 
 log="时间: `date '+%Y-%m-%d %H:%M:%S'` UTC \n主机名: `hostname` \n节点: $service \n状态: 快照已完成。"
 echo -e $log >> $log_file
